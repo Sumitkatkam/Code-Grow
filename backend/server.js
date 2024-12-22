60bccc55ab7149600ab276ac7d4c401a9abce266
Sumitkatkam <sumitkatkam@gmail.com>
2024-12-22 20:50:52 +0530

    Created backend folder

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const ACTIONS = require('./src/Actions');
+const ACTIONS = require('../src/Actions');
 const app = express();
 const path = require('path');
 const http = require('http');
@@ -82,5 +82,5 @@ io.on('connection', socket => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
