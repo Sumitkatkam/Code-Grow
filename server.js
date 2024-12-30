d79fdcf4f21cfc53f033f3feb4900d182414e27f
Sumitkatkam <sumitkatkam@gmail.com>
2024-12-30 23:50:53 +0530

    Removed the backend folder

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const ACTIONS = require('../src/Actions');
+const ACTIONS = require('./src/Actions');
 const app = express();
 const path = require('path');
 const http = require('http');
